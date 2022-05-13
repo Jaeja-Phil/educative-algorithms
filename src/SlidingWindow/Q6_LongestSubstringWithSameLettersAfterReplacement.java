ebdddd7dbc0bc10d70313ee8722d3a559d679bc7
phil <jaeja.phil@gmail.com>
2022-05-13 17:30:26 +0900

    comment sliding window question 6: longest substring with same letter after replacement

--- src/SlidingWindow/Q6_LongestSubstringWithSameLettersAfterReplacement.java
+++ src/SlidingWindow/Q6_LongestSubstringWithSameLettersAfterReplacement.java
@@ -14,19 +14,32 @@ public class Q6_LongestSubstringWithSameLettersAfterReplacement {
     }
 
     public static int solution(String str, int k) {
-        int left = 0, maxLen = 0, maxRepeatLetterCount = 0;
+        // initialize left side of the window
+        int left = 0;
+        // create a variable maxLen to keep track of the max length of same letter substring after replacement
+        int maxLen = 0;
+        // create a variable to keep track of the max repeating letter
+        // - this variable does not need to keep track of "current window"'s max repeating letter count
+        //   rather, keep the max repeating letter so far because we are only interested in max substring (window) size
+        int maxRepeatLetterCount = 0;
+        // create a hashmap to store frequency of character occurrences
         Map<Character, Integer> characterFrequencyMap = new HashMap<>();
+        // initialize and start extending window to the right
         for (int right = 0; right < str.length(); right++) {
+            // pull out character from the right side of the window
             Character rightChar = str.charAt(right);
+            // and record it to characterFrequencyMap
             characterFrequencyMap.put(rightChar, characterFrequencyMap.getOrDefault(rightChar, 0) + 1);
+            // compare & replace maxRepeateLetterCount
             maxRepeatLetterCount = Math.max(maxRepeatLetterCount, characterFrequencyMap.get(rightChar));
-
+            // if curent window size - max repeat letter count is greater than k, all replacements have been made
+            // and the window needs to shrink
             if (right - left + 1 - maxRepeatLetterCount > k) {
                 Character leftChar = str.charAt(left);
                 characterFrequencyMap.put(leftChar, characterFrequencyMap.get(leftChar) - 1);
                 left++;
             }
-
+            // compare & replace maxLen with current window size
             maxLen = Math.max(maxLen, right - left + 1);
         }
 
